deb1d8717d55196e68961bc5a660f247d0ff319d
Aliaa Ibrahem <aliaa.ebrahem21@gmail.com>
2022-11-30 21:35:41 +0200

    create Consultation database entity

---
+++ Backend/online-Pharmacy/src/main/java/com/example/onlinePharmacy/Model/Consultation.java
@@ -0,0 +1,17 @@
+package com.example.onlinePharmacy.Model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Consultation {
+    @Id
+    @Column(name = "diagnosis", nullable = false)
+    private String diagnosis;
+
+    @OneToMany(targetEntity = Product.class)
+    private List<Integer> productId;
+
+
+}
