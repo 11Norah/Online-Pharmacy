b3756d5ae397a1ab21ca53ba73973125ab3c7b1e
Engy <engeousthepotionmaster@gmail.com>
2022-12-02 04:54:48 +0200

    fix bugs

--- Backend/src/main/java/com/example/onlinePharmacy/Repositries/ProductRepo.java
+++ Backend/src/main/java/com/example/onlinePharmacy/Repositries/ProductRepo.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ProductRepo extends CrudRepository<Product, Long> {
-    public List<Product> findAllByCategory(String category);
+    List<Product> findAllByCategory(String category);
 }
