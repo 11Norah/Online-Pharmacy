225ae5838aba411414eac1290a3a0d7886157238
Engy <engeousthepotionmaster@gmail.com>
2022-12-02 02:54:28 +0200

    add service and controller for sign up and in

--- src/main/java/com/example/onlinePharmacy/Repositries/UserRepo.java
+++ src/main/java/com/example/onlinePharmacy/Repositries/UserRepo.java
@@ -1,12 +1,11 @@
 package com.example.onlinePharmacy.Repositries;
 
-import org.apache.catalina.User;
+import com.example.onlinePharmacy.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User,Long> {
-    @Query("SELECT u FROM User u WHERE u.userName = ?1")
+
     Optional<User> findUserByEmail(String email) ;
 }
