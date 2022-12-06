ebb95ee429ffd81ffc0de7497804c939eb4e1af4
Norah Ahmed <95590883+11Norah@users.noreply.github.com>
2022-12-06 11:22:23 +0200

    Removing excess imports

--- Backend/src/test/java/com/example/onlinePharmacy/Controllers/ProductControllerTest.java
+++ Backend/src/test/java/com/example/onlinePharmacy/Controllers/ProductControllerTest.java
@@ -3,7 +3,7 @@ package com.example.onlinePharmacy.Controllers;
 import com.example.onlinePharmacy.DTOs.ProductDto;
 import com.example.onlinePharmacy.Services.ProductService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
