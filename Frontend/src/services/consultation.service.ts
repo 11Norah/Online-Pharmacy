ce05d79a5d48f40123a2b44713051e37f783ffd9
Aliaa Ibrahem <90799411+AliaaIbrahemAhmed@users.noreply.github.com>
2022-12-17 12:15:57 +0200

    try to use https and ssl

--- Frontend/src/services/consultation.service.ts
+++ Frontend/src/services/consultation.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from 'src/models/product.model';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConsultationService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public getTreatments(diagnosis: string): Observable<Product[]> {
-    return this.http.get<Product[]>("http://localhost:8080/consultations?diagnosis=" + diagnosis);
+    return this.http.get<Product[]>(
+      'https://localhost:443/consultations?diagnosis=' + diagnosis
+    );
   }
 }
