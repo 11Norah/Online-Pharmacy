02250becbf65834de9090e6e61e1046420a355ba
Aliaa Ibrahem <90799411+AliaaIbrahemAhmed@users.noreply.github.com>
2022-12-17 19:25:25 +0200

    Encrypt password before storing in database

--- Backend/src/main/java/com/example/onlinePharmacy/Controllers/SignUpInController.java
+++ Backend/src/main/java/com/example/onlinePharmacy/Controllers/SignUpInController.java
@@ -2,22 +2,34 @@ package com.example.onlinePharmacy.Controllers;
 
 import com.example.onlinePharmacy.DTOs.SignInDto;
 import com.example.onlinePharmacy.DTOs.UserDto;
+import com.example.onlinePharmacy.Mappers.UserMapper;
+import com.example.onlinePharmacy.Model.User;
+import com.example.onlinePharmacy.Repositries.UserRepo;
 import com.example.onlinePharmacy.Services.SignUpInService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 public class SignUpInController {
     @Autowired
     SignUpInService service;
+    @Autowired
+    UserRepo userRepo;
+
 
     @PostMapping("/signup")
     public boolean signUp(@RequestBody UserDto user) {
         return service.recordNewUser(user);
     }
-
-    @PostMapping("/signin")
+     @PostMapping("/signin")
     public boolean signIn(@RequestBody SignInDto signInDto) {
         return service.authenticate(signInDto);
+    }
+
+    @GetMapping(path = "/getAllUsers")
+    public @ResponseBody List<UserDto> getAllProducts() {
+        List<UserDto> userDtos = UserMapper.bulkMappingFromUserToDto((List<User>) userRepo.findAll());
+        return userDtos;
     }
 }
