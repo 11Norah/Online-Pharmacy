b6e9a0be83f204c3510662591d9c8e74cfd528f8
Norah Ahmed <95590883+11Norah@users.noreply.github.com>
2022-12-21 15:08:54 +0200

    Remove confirmation timer

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -59,7 +59,6 @@ import { RegistrationComponent } from './registration/registration.component';
 
     ,{path:"shoppingcart",component:ShoppingcartComponent},{path:"aboutproduct",component:AboutProductComponent}
     ,{path:"registration",component:RegistrationComponent} , {path:"SelectedCategory",component:SelectedCategoryComponent},{path:"confirm",component:ConfirmationComponent}
-
    , {path:"profile",redirectTo:'profile/myinfo',pathMatch:'full'},{path:"profile/myinfo",component:MyinfoComponent}])
   ],
   providers: [],
