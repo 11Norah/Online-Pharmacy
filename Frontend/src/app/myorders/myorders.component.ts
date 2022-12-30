ebdcae66a9cc3326f1b193dd145935fb0dafc07b
mariamtarek51 <mariamtarek51@gmail.com>
2022-12-31 00:41:02 +0200

    fix cost label

--- Frontend/src/app/myorders/myorders.component.ts
+++ Frontend/src/app/myorders/myorders.component.ts
@@ -42,14 +42,14 @@ export class MyordersComponent implements OnInit {
       for(var j=0;j<getOccurrence(this.myorders,ProductsInOrder);j++){ 
         if(j==0){
           (document.getElementById('orderscontent')as HTMLBodyElement)
-      .innerHTML +='<div style=" ">'+'<strong  style="color:rgb(1, 1, 1) ;   text-shadow: 0 0 3px rgb(249, 208, 24), 0 0 5px rgb(249, 208, 24); font-size: x-large; display:flex;">'+"Order #"+counter+"</strong>"+"<br>"+'<p style=" font-size: xx-large; font-weight:bold;">   Date : '+this.myorders[i].orderTime +'  (Eastern West Egypt)';
+      .innerHTML +='<div style=" ">'+'<strong  style="color:rgb(1, 1, 1) ;   text-shadow: 0 0 3px rgb(249, 208, 24), 0 0 5px rgb(249, 208, 24); font-size: xx-large; display:flex;">'+"Order #"+counter+"</strong>"+"<br>"+'<p style=" font-size: x-large; font-weight:bold;">   Date : '+this.myorders[i].orderTime +'  (Eastern European Standard Time)';
 
         }
-        (document.getElementById('orderscontent')as HTMLBodyElement).innerHTML +='<br>'+'<br>'+'<div style="width:300px;height:400px;background-color: white; padding:0px;border-radius: 15px;box-shadow: 0px 0px 15px 2px rgb(249, 208, 24);transition: 0.5s;margin-bottom: 70px;">'
+        (document.getElementById('orderscontent')as HTMLBodyElement).innerHTML +='<br>'+'<br>'+'<div style="width:300px;height:410px;background-color: white; padding:0px;border-radius: 15px;box-shadow: 0px 0px 15px 2px rgb(249, 208, 24);transition: 0.5s;margin-bottom: 70px;">'
         +'<div style ="width=300; height=200;">'+'<img style="object-fit: cover; border-radius: 15px; position: relative; width:300px;  height:250px;" src='
-        +this.myorders[i+j].imageUrl+"></div>"+this.myorders[i+j].productName +"<br>"+"<br>"+'<span style="color:white;  background-color: rgb(113, 113, 113); border: 2px solid black ;padding: 10px; border-radius: 25px;">Quantity</span>'
+        +this.myorders[i+j].imageUrl+"></div>"+this.myorders[i+j].productName +"<br>"+"<br>"+'<span style="color:white;  margin-left: 5px; background-color: rgb(113, 113, 113); border: 2px solid black ;padding: 10px; border-radius: 25px;">Quantity</span>'
         +" : "+  this.myorders[i+j].quantity
-        +"<br>"+"<br>"+"<br>"+'<span style="color:white;  background-color: rgb(113, 113, 113); border: 2px solid black ;padding: 10px; border-radius: 25px;">Cost</span>'
+        +"<br>"+"<br>"+"<br>"+' <span style="color:white;  background-color: rgb(113, 113, 113); border: 2px solid black ;padding: 15px; margin: 5px;  border-radius: 25px;">Cost</span>'
         +" : "+this.myorders[i+j].cost+" LE"+'</div>';
 
         if(j==ProductsInOrder-1){
