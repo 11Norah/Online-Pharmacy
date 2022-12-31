a25a6af440b3e8bd5c34ea09b0df17793b0909b1
Aliaa <aliaaf518@gmail.com>
2022-12-31 04:37:31 +0200

    handling Empty Cart

--- Frontend/src/app/shoppingcart/shoppingcart.component.ts
+++ Frontend/src/app/shoppingcart/shoppingcart.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { Product } from 'src/models/product.model';
 import { ProductService } from 'src/services/product.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-shoppingcart',
   templateUrl: './shoppingcart.component.html',
   styleUrls: ['./shoppingcart.component.css']
 })
 export class ShoppingcartComponent {
-  constructor(private Server:ProductService){}
+  constructor(private Server:ProductService,private router:Router){}
   
   subtotal=JSON.parse(localStorage.getItem("subtotal")!);
   Total=JSON.parse(localStorage.getItem("subtotal")!)+20;
@@ -82,7 +84,13 @@ removeItemFromCart(id:any){
       localStorage.setItem("subtotal",JSON.stringify(this.subtotal))
       productInCart.splice(i,1);
       localStorage.setItem("CartProducts",JSON.stringify(productInCart));
+      console.log("productsssss"+productInCart.length);
       this.bestsellerproducts=productInCart;
+      if(productInCart.length==0){
+        this.router.navigate(['/bestseller'])
+
+      }
+      
     }
   }
   
@@ -100,7 +108,7 @@ yes(){
   let dateTime = new Date();
   let temp:{UserID:Number,productId:Number,quantity:Number,time:Date}={UserID:0,productId:0,quantity:0,time:dateTime}
   let productInCart:{product_id:number,image:string,name:string,price:number,duplication:number}[]=[];
-  temp.UserID=JSON.parse(localStorage.getItem("UserId")!);
+  //temp.UserID=JSON.parse(localStorage.getItem("UserId")!);
   temp.time=dateTime;
   productInCart=JSON.parse(localStorage.getItem("CartProducts")!);
   for(var i=0;i<productInCart.length;i++){
@@ -108,6 +116,15 @@ yes(){
     temp.quantity=productInCart[i].duplication
     listDto.push(temp);
   }
+  let p:Product[]=[];
+  localStorage.setItem("CartProducts",JSON.stringify(p));
+  console.log(localStorage.getItem("CartProducts"))
+  localStorage.setItem('itemsincart',"0");
+  localStorage.setItem("subtotal","0")
+  this.router.navigate(['/bestseller'])
+ 
+  
+
   //call back*/
 }
 clossing(){
